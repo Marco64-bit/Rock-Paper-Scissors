26af583c4e39417c8a9dd3a291dddb992718f852
Marco Sameh Samir Said <marco240100851@sut.edu.eg>
2025-06-11 11:47:04 +0300

    first commit

---
+++ rock_paper_scissors_lizard_spock.py
@@ -0,0 +1,69 @@
+import random
+player = input("================================\nnRock Paper Scissors\n================================\n\n1) ✊\n2) ✋\n3) ✌️\n4) 🦎\n5) 🖖\nPick a number: ")
+computer = random.randint(1, 5)
+if player == "1":
+    player = "✊"
+elif player == "2":
+    player = "✋"
+elif player == "3":
+    player = "✌️"
+elif player == "4":
+    player = "🦎"
+elif player == "5":
+    player = "🖖"
+
+if computer == 1:
+    computer = "✊"
+elif computer == 2:
+    computer = "✋"
+elif computer == 3:
+    computer = "✌️"
+elif computer == 4:
+    computer = "🦎"
+elif computer == 5:
+    computer = "🖖"
+
+if player == "✊" and computer == "✋":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe CPU won!")
+elif player == "✊" and computer == "✌️":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe player won!")
+elif player == "✊" and computer == "🦎":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe player won!")
+elif player == "✊" and computer == "🖖":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe CPU won!")
+elif player == "✋" and computer == "✊":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe player won!")
+elif player == "✋" and computer == "✌️":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe CPU won!")
+elif player == "✋" and computer == "🦎":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe CPU won!")
+elif player == "✋" and computer == "🖖":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe player won!")
+elif player == "✌️" and computer == "✊":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe CPU won!")
+elif player == "✌️" and computer == "✋":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe player won!")
+elif player == "✌️" and computer == "🦎":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe player won!")
+elif player == "✌️" and computer == "🖖":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe CPU won!")
+elif player == "🦎" and computer == "✊":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe CPU won!")
+elif player == "🦎" and computer == "✋":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe player won!")
+elif player == "🦎" and computer == "✌️":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe CPU won!")
+elif player == "🦎" and computer == "🖖":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe player won!")
+elif player == "🖖" and computer == "✊":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe player won!")
+elif player == "🖖" and computer == "✋":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe CPU won!")
+elif player == "🖖" and computer == "✌️":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe player won!")
+elif player == "🖖" and computer == "🦎":
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nThe CPU won!")
+elif player == computer:
+    print(f"\nYou chose: {player}\nCPU chose: {computer}\nIt's a tie!")
+else:
+    print("\nInvalid input! Please choose a number between 1 and 3.")
